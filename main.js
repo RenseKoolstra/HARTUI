dec31d44eefaee889553c435424d0a86be27d6db
Rense <rense@ndus3.com>
2025-02-12 14:00:11 +0100

    cypress installed, selectParameters done

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 //Global constants
-const csvAsJson = {};
+const parameters = document.querySelector("#parameters");
 
 //Read csv
 document.getElementById("csvFile").addEventListener("change", async function(event) {
     const file = event.target.files[0];
-    await readCsv(file);
+    csvAsJson = await readCsv(file);
+    displayParameters(csvAsJson);
     
 });
 
 async function readCsv(file)   {
+    return new Promise((resolve, reject) => {
     if (!file) return;
-
+    
     const reader = new FileReader();
+    const csvAsJson = {}
     reader.onload = async function(e) {
         const text = e.target.result;
         const lines = text.split("\n").map(line => line.trim()).filter(line => line); // Remove empty lines
@@ -34,14 +37,39 @@ async function readCsv(file)   {
                 csvAsJson[header].push(values[index] || ""); // Handle missing values
             });
         }
+        resolve(csvAsJson)
     };
+reader.onerror = (error) => reject(error);
+reader.readAsText(file);
+});
 
-    reader.readAsText(file);
+    
+    
+
 }
 
-function displayParameters() {
-    if (JSON.stringify(csvAsJson) === '{}') return; // check if jsonobject is empty. 
+function displayParameters(csvAsJson) {
+    if (JSON.stringify(csvAsJson) === '{}') {console.log('DisplayParamters returned'); return;} // check if jsonobject is empty. 
+
     for (const key in csvAsJson) {
-        
+        console.log(key);
+        //for each key(parameter in the json object create a div with parameter class)
+        const new_parameter = document.createElement("div");
+        new_parameter.classList.add('parameter');
+
+
+        //for each div with parameter class create a parameter label with the key as text and parameter_value as class. 
+        const new_parameter_value = document.createElement("p");
+        new_parameter_value.textContent = key;
+        new_parameter_value.classList.add('parameter_value');
+        new_parameter.appendChild(new_parameter_value);
+
+        //for each div with parameter class add a checkbox with class parameter_checkbox.
+        const new_parameter_checkbox = document.createElement("input");
+        new_parameter_checkbox.setAttribute("type", "checkbox")
+        new_parameter_checkbox.classList.add('parameter_check');
+        new_parameter.appendChild(new_parameter_checkbox);
+
+        parameters.appendChild(new_parameter);
     }
 }
