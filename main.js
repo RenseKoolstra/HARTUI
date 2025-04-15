108a21491fc238709366568341b4ffbc2725f865
Rense <rense@ndus3.com>
2025-04-15 14:55:08 +0200

    updated comments

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 //Global constants
+
+//DOM elements
 const parameters = document.querySelector("#parameters");
 const simpleChart = document.querySelector("#simpleChart");
 const display = document.querySelector('#DisplayOptions');
 const lightdarkemoji = document.querySelector('#lightdarkemoji')
 const addParameter = document.querySelector('#calcModal')
+
+//Backend variables
 var displaymode = display.value;
 var csvAsJson = {};
 var t = {};
@@ -11,32 +15,35 @@ var yParameters = {}
 var datalist = [];
 var parameterlist = [];
 var myChart;
-var prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+var prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;  //lookup light dark settings of windows.
+lightdarkemoji.textContent = prefersDarkScheme ? '🌙': '☀️';    //Set emoji of light dark mode to the one of windows setting
 
-lightdarkemoji.textContent = prefersDarkScheme ? '🌙': '☀️';
-
-
+//function that changes dark light mode
 function changelightdark() {
-    prefersDarkScheme = prefersDarkScheme ? 0:1;
+prefersDarkScheme = prefersDarkScheme ? 0:1;
     document.documentElement.style.setProperty('color-scheme', prefersDarkScheme ? "dark":"light");
     lightdarkemoji.textContent = prefersDarkScheme ? "🌙":"☀️";
     displaygraph(t['time [s]'], datalist, parameterlist);
 }
 
-lightdarkemoji.addEventListener("click", changelightdark);
+lightdarkemoji.addEventListener("click", changelightdark); //event listener for dark light mode when is clicked on emoji.
+
+//event lister for dark light mode for when windows setting is change. 
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
     if (window.matchMedia("(prefers-color-scheme: dark)").matches != prefersDarkScheme) {
         changelightdark();
     }
 });
 
-//add eventlisteners for all mathOperators
+//add eventlisteners for all mathOperators that are used in calcUI
 const mathOperators = document.querySelectorAll('.mathOperator').forEach(mathOperator => {
     mathOperator.addEventListener('click', () => {
         document.getElementById('newParameterEquation').value +=  mathOperator.textContent
     })
 })
 
+
+//eventlistener for when display mode is changed. 
 display.addEventListener("change", async function() {
     displaymode = display.value;
     displaygraph(t['time [s]'], datalist, parameterlist);
@@ -97,16 +104,16 @@ async function getTime()   {
     } 
     yParameters = {...csvAsJson};
     delete yParameters['Log Timestamp [498]'];
-}
+    }
 
-//display csv parameters
-function displayParameters() {
+    //display csv parameters
+    function displayParameters() {
     //delete all old parameters
     const oldParameters = document.querySelectorAll(".parameter");
     oldParameters.forEach((oldParamter) => {
         oldParamter.remove();
     });
-    
+
 
     //create new parameters
     if (JSON.stringify(yParameters) === '{}') {console.log('DisplayParamters returned'); return;} // check if jsonobject is empty. 
@@ -143,7 +150,7 @@ function displayParameters() {
                 if (checkbox.checked)  {
                     datalist.push(yParameters[checkbox.getAttribute("value")]);
                     parameterlist.push(checkbox.getAttribute("value"));
-                 
+                    
                 }
 
             })
@@ -153,7 +160,6 @@ function displayParameters() {
 }
 
 //display createParameterButton
-
 function displayCreateParameterButton()   {
     if (document.querySelector('#createParameterButton') !== null) {
         return
@@ -164,7 +170,7 @@ function displayCreateParameterButton()   {
         createParameterButton.setAttribute('id', "createParameterButton");
         createParameterButtonBox.appendChild(createParameterButton);
         createCreateParameterEventListener();
-    }
+}
 }
 
 
@@ -188,55 +194,62 @@ function createCreateParameterEventListener()    {
                 document.getElementById('newParameterEquation').value +=  key
             })
         }
-        addParameter.style.display = 'flex'; // show ui
-        //calculate equation
+        addParameter.style.display = 'flex'; // show calcUI
+        //eventlistener for submit button
         document.getElementById('submitNewParameter').addEventListener("click", createNewParameter)
     })        
 }
 
+//function that calculates new parameter
 function createNewParameter() {
+    //get new parameter name and equation.
     newParameterName = document.getElementById('newParameterName').value;
-            equation = document.getElementById('newParameterEquation').value;
-            if (newParameterName === null || newParameterName === ''|| equation === null || equation === '') {
-                alert("No input given");
-                addParameter.style.display = 'none';
-                document.getElementById('submitNewParameter').removeEventListener('click', createNewParameter)
-                return;
-            }else if (newParameterName in yParameters){
-                alert("Parameter name already exist");
-                return;
-            } else {
-                try {
-                    const result = [];
-                    for (var i = 0; i < t['time [s]'].length; i++) {
-                        const scope = {};
-                        let equation_t = equation
-                        let j = 0;
-                        for (let key in yParameters) {
-                            if (equation.includes(key.toString())) {
-                                j++
-                                equation_t = equation_t.replace(new RegExp(escapeRegex(key), 'g'), `var${j}_${i}`)
-                                Object.assign(scope, {[`var${j}_${i}`]: yParameters[key][i]});
-                            }                
-                        }
-                        result.push(math.evaluate(equation_t, scope));
-                    }
-                    //check if new parameter has strange numbers like inf or NAN and give a warning. The new parameter is still added and can be used.
-                    const hasInvalid = result.some(n => !Number.isFinite(n));
-                    if (hasInvalid) {
-                        alert("New parameter contains values that can't be displayed by the graph like inf or NAN. Check for possibility of divide by 0 or if datasets are used that didn't contain numbers.");
-                    }
-                    Object.assign(yParameters, {[newParameterName]: result})
-                    displayParameters()
-                } catch (error) {
-                    console.error("Invalid input:", error.message);
-                    alert("Error: Invalid mathematical expression! Please try again.");
-                    addParameter.style.display = 'none';
-                    return;
+    equation = document.getElementById('newParameterEquation').value;
+    //check if new parameter name and equation have values
+    if (newParameterName === null || newParameterName === ''|| equation === null || equation === '') {
+        alert("No input given");
+        addParameter.style.display = 'none';
+        document.getElementById('submitNewParameter').removeEventListener('click', createNewParameter)
+        return;
+    }else if (newParameterName in yParameters){             //check if name of new parameter is not already used.
+        alert("Parameter name already exist");
+        return;
+    } else {
+        // if name and equation check out try to calculate new parameter
+        try {
+            const result = [];
+            for (var i = 0; i < t['time [s]'].length; i++) {
+                const scope = {};
+                let equation_t = equation
+                let j = 0;
+                for (let key in yParameters) {
+                    if (equation.includes(key.toString())) {
+                        j++
+                        equation_t = equation_t.replace(new RegExp(escapeRegex(key), 'g'), `var${j}_${i}`)
+                        Object.assign(scope, {[`var${j}_${i}`]: yParameters[key][i]});
+                    }                
                 }
-                
+                result.push(math.evaluate(equation_t, scope));
             }
+            //check if new parameter has strange numbers like inf or NAN and give a warning. The new parameter is still added and can be used.
+            const hasInvalid = result.some(n => !Number.isFinite(n));
+            if (hasInvalid) {
+                alert("New parameter contains values that can't be displayed by the graph like inf or NAN. Check for possibility of divide by 0 or if datasets are used that didn't contain numbers.");
+            }
+            //ad new parameter to yParameter object
+            Object.assign(yParameters, {[newParameterName]: result})
+            //refresh parameterlist
+            displayParameters()
+        } catch (error) {
+            console.error("Invalid input:", error.message);
+            alert("Error: Invalid mathematical expression! Please try again.");
             addParameter.style.display = 'none';
+            return;
+        }
+        
+    }
+    // hide calcUI
+    addParameter.style.display = 'none';
 }
 //create myChart object
 function displaygraph(tValues, yValues, parameter) {
@@ -249,150 +262,135 @@ function displaygraph(tValues, yValues, parameter) {
         var gridColor = 'rgb(70, 70, 70)';
         var textcolor = 'rgb(250, 250, 250)';
     }
-    
-     if (myChart) {
-         myChart.destroy();
-         myChart = null; // Clear reference
-     }
-     let multiplescales = {
-         'x': {
-             title: {
-                 display: true,
-                 text: 'Time (s)',
-                 color: textcolor
-             },
 
-             grid: {
-                color: gridColor,
-             },
-             ticks: {
+    if (myChart) {
+        myChart.destroy();
+        myChart = null; // Clear reference
+    }
+
+    let multiplescales = {
+        'x': {
+            title: {
+                display: true,
+                text: 'Time (s)',
                 color: textcolor
-             }
+            },
+            grid: {
+            color: gridColor,
+            },
+            ticks: {
+            color: textcolor
+            }       
+        }
+    }
 
-           
-         }
-     }
- 
- 
-     for (i=0; i < yValues.length; i++) {
-         dsets[i] = {
-             label: parameter[i],
-             data: yValues[i],
-             borderWidth: 2,
-             fill: false,
-             yAxisID: `y${i}`,
-             pointStyle: 'circle',
-             pointRadius: 1,
-             pointHoverRadius: 6, 
-         };
+
+    for (i=0; i < yValues.length; i++) {
+        dsets[i] = {
+            label: parameter[i],
+            data: yValues[i],
+            borderWidth: 2,
+            fill: false,
+            yAxisID: `y${i}`,
+            pointStyle: 'circle',
+            pointRadius: 1,
+            pointHoverRadius: 6, 
+        };
         switch(displaymode)    {
             case "fully stacked":
-                multiplescales[`y${i}`] = 
-                    {
-                        
-                        title: {
-                            display: true,
-                            text: parameter[i],   
-                            color: textcolor 
-                        },
-                       
-                        type: 'linear',
-                        offset: true,
-                        position: 'left',                        
-                        stack: 'demo',
-                        grid: {
-                            color: gridColor
-                        },
-                        ticks: {
-                            color: textcolor
-                         }
-            
-            
-                    }
-                    ylimits[`y${i}`] = {
-                        min: Math.min(...yValues[i]) - (Math.max(...yValues[i]) - Math.min(...yValues[i]))*0.1,
-                        max: Math.max(...yValues[i]) + (Math.max(...yValues[i]) + Math.min(...yValues[i]))*0.1  
-                    }
+                multiplescales[`y${i}`] = {                        
+                    title: {
+                        display: true,
+                        text: parameter[i],   
+                        color: textcolor 
+                    },
                     
-
-                    break;
+                    type: 'linear',
+                    offset: true,
+                    position: 'left',                        
+                    stack: 'demo',
+                    grid: {
+                        color: gridColor
+                    },
+                    ticks: {
+                        color: textcolor
+                        }
+                }
+                ylimits[`y${i}`] = {
+                    min: Math.min(...yValues[i]) - (Math.max(...yValues[i]) - Math.min(...yValues[i]))*0.1,
+                    max: Math.max(...yValues[i]) + (Math.max(...yValues[i]) + Math.min(...yValues[i]))*0.1  
+                }
+                break;
             case "semi stacked":
-                multiplescales[`y${i}`] = 
-                    {
-                        display: false,
-                        title: {
-                            display: true,
-                            text: parameter[i],
-                            color: textcolor 
+                multiplescales[`y${i}`] = {
+                    display: false,
+                    title: {
+                        display: true,
+                        text: parameter[i],
+                        color: textcolor 
+                    },
+                    grid: {
+                        color: gridColor
+                    },
+                    ticks: {
+                        color: textcolor
                         },
-                        grid: {
-                            color: gridColor
-                        },
-                        ticks: {
-                            color: textcolor
-                         },
-            
-                        type: 'linear',
-                        offset: true,
-                        position: 'left',
-
-                        
-                        //stack: FALSE,
-                        min: Math.min(...yValues[i]) - i*(Math.max(...yValues[i]) - Math.min(...yValues[i])),
-                        max: Math.max(...yValues[i]) + (yValues.length -1 -i)*(Math.max(...yValues[i]) - Math.min(...yValues[i]))            
-                        
-                    }
-                    ylimits[`y${i}`] = {
-                        min: Math.min(...yValues[i]) - i*(Math.max(...yValues[i]) - Math.min(...yValues[i])),  
-                        max: Math.max(...yValues[i]) + (yValues.length -1 -i)*(Math.max(...yValues[i]) - Math.min(...yValues[i]))
-                    }
-                    break;
-            case "not stacked": 
-                multiplescales[`y${i}`] = 
-                    {
-                        display: false,
-                        title: {
-                            display: true,
-                            text: parameter[i],  
-                            color: textcolor
-                        },
-                        grid: {
-                            color: gridColor
-                        },
-                        ticks: {
-                            color: textcolor
-                         },
-            
-                        type: 'linear',
-                        offset: true,
-                        position: 'left',
-
+        
+                    type: 'linear',
+                    offset: true,
+                    position: 'left',
+                
+                    //stack: FALSE,
+                    min: Math.min(...yValues[i]) - i*(Math.max(...yValues[i]) - Math.min(...yValues[i])),
+                    max: Math.max(...yValues[i]) + (yValues.length -1 -i)*(Math.max(...yValues[i]) - Math.min(...yValues[i]))            
                     
-                        //stack: FALSE,
-                        min: Math.min(...yValues[i]) - (Math.max(...yValues[i]) - Math.min(...yValues[i]))*0.1,
-                        max: Math.max(...yValues[i]) + (Math.max(...yValues[i]) - Math.min(...yValues[i]))*0.1            
-                    }
-                    ylimits[`y${i}`] = {
-                        min: Math.min(...yValues[i]) - (Math.max(...yValues[i]) - Math.min(...yValues[i]))*0.1,
-                        max: Math.max(...yValues[i]) + (Math.max(...yValues[i]) - Math.min(...yValues[i]))*0.1 
+                }
+                ylimits[`y${i}`] = {
+                    min: Math.min(...yValues[i]) - i*(Math.max(...yValues[i]) - Math.min(...yValues[i])),  
+                    max: Math.max(...yValues[i]) + (yValues.length -1 -i)*(Math.max(...yValues[i]) - Math.min(...yValues[i]))
+                }
+                break;
+            case "not stacked": 
+                multiplescales[`y${i}`] = {
+                    display: false,
+                    title: {
+                        display: true,
+                        text: parameter[i],  
+                        color: textcolor
+                    },
+                    grid: {
+                        color: gridColor
+                    },
+                    ticks: {
+                        color: textcolor
+                        },
+        
+                    type: 'linear',
+                    offset: true,
+                    position: 'left',
+
+                
+                    //stack: FALSE,
+                    min: Math.min(...yValues[i]) - (Math.max(...yValues[i]) - Math.min(...yValues[i]))*0.1,
+                    max: Math.max(...yValues[i]) + (Math.max(...yValues[i]) - Math.min(...yValues[i]))*0.1            
+                }
+                ylimits[`y${i}`] = {
+                    min: Math.min(...yValues[i]) - (Math.max(...yValues[i]) - Math.min(...yValues[i]))*0.1,
+                    max: Math.max(...yValues[i]) + (Math.max(...yValues[i]) - Math.min(...yValues[i]))*0.1 
                 }
         }
     }
-    
+
     myChart = new Chart(simpleChart, {
         type: 'line',
         data: {
             labels: tValues,
-            datasets: dsets,
-            
+            datasets: dsets,    
         },
         options: {
-            
             responsive: true,
             animation: false,
-            
             scales : multiplescales,
-            
             plugins: {
                 legend: {
                     labels: {color: textcolor}
@@ -400,7 +398,7 @@ function displaygraph(tValues, yValues, parameter) {
                 tooltip: {
                     enabled: true
                 },
-               
+                
                 zoom: {
                     limits: ylimits,
                     pan: {
@@ -410,8 +408,8 @@ function displaygraph(tValues, yValues, parameter) {
                             const w10 = area.width * 0.1;
                             const h10 = area.height * 0.1;
                             if (point.x < area.left + w10 || point.x > area.right - w10
-                               || point.y < area.top + h10 || point.y > area.bottom - h10) {
-                               return false; // abort
+                                || point.y < area.top + h10 || point.y > area.bottom - h10) {
+                                return false; // abort
                             }
                         },
                         mode: 'xy', // Enable panning on the x-axis
